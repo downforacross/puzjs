563f0dd014952fa30c5b589635e5f2e952593cf7
stevenhao <stevenhao@gmail.com>
2018-08-19 11:02:16 -0700

    add babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp');
 var umd = require('gulp-umd');
+var babel = require('gulp-babel');
 
 gulp.task('default', function() {
   return gulp.src('src/puz.js')
     .pipe(umd())
+    .pipe(babel({
+        presets: ['env']
+    }))
     .pipe(gulp.dest('.'));
+
 });
 
